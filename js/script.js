d326917e180a59819e95fdf37c0801e6a96e17f5
Paola Arrieta <vargasp437@gmail.com>
2024-03-24 16:42:53 -0600

    cambio

--- js/script.js
+++ js/script.js
@@ -118,3 +118,16 @@ function cambiarImagen() {
     }
 
 };
+
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
+nextButton.addEventListener("click", (event) => {
+    const slideWidth = slide.clientWidth;
+      slidesContainer.scrollLeft += slideWidth;
+  });
+  prevButton.addEventListener("click", () => {
+    const slideWidth = slide.clientWidth;
+    slidesContainer.scrollLeft -= slideWidth;
+  });
